2e6a4407d5a09e9503cc47f8021abcbfaa8632c4
murilo <61051754+muriloamancio00@users.noreply.github.com>
2023-09-19 16:17:02 -0300

    final AbstractRepository, Category, Product

--- app/Repositories/ProductRepository.php
+++ app/Repositories/ProductRepository.php
@@ -1,38 +1,7 @@
 <?php
 
 namespace App\Repositories;
-
-use Illuminate\Database\Eloquent\Model;
-
-class ProductRepository{
-
-    public function __construct(Model $model){
-        $this->model = $model;
-    }
-
-    public function selecAtributosRegistrosRelacionados($atributos) {
-        $this->model = $this->model->with($atributos);
-        //query sendo montada
-    }
-
-    public function filtro($filtros) {
-
-        $filtros = explode(';',$filtros);
-
-        foreach($filtros as $key=> $condicao) {
-            $c = explode(':',$condicao);
-            $this->model = $this->model->where($c[0],$c[1],$c[2]);
-            //atualizando query
-        }
-    }
-
-    public function selectAtributos($atributos){
-        $this->model = $this->model->selectRaw($atributos);
-    }
-
-    public function getResultado() {
-        return $this->model->get();
-    }
+class ProductRepository extends AbstractRepository {
 
 }
 ?>
