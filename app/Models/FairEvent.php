12959d4fba2cdd7ab1550ed2750f7c97c8410dfb
murilo <61051754+muriloamancio00@users.noreply.github.com>
2023-10-02 10:14:31 -0300

    Model e Repository FairsEvents

--- app/Models/FairEvent.php
+++ app/Models/FairEvent.php
@@ -12,6 +12,30 @@ class FairEvent extends Model
     protected $table = 'fairs_events';
     protected $fillable = ['fair_id', 'event_id', 'data', 'status', 'horarioInicio', 'horarioFim'];
 
+    public function rules() {
+        return [
+            'fair_id' => 'required|integer|not_in:0|unique:fairs',
+            'event_id' => 'required|integer|not_in:0|unique:events',
+            'data' => 'required|date_format:Y-m-d',
+            'status' => 'required|integer|in:1,2,3,4',
+            'horarioInicio' => 'required|regex:/^[0-9]{2}:[0-9]{2}$/|before_or_equal:horarioFim', // Adicionando filtro e regra para horarioInicio.
+            'horarioFim' => 'required|regex:/^[0-9]{2}:[0-9]{2}$/|after_or_equal:horarioInicio', // Adicionando filtro e regra para horarioFim.
+        ];
+    }
+    public function feedback() {
+        return [
+            'required' => 'O campo :attribute é obrigatório',
+            'unique' => 'O :attribute já está em uso',
+            'integer' => 'O :attribute deve ser um número inteiro',
+            'date_format' => 'O :attribute deve estar no formato YYYY-MM-DD',
+            'in' => 'O :attribute deve ter um dos valores: :values', // Feedback para tipos de status.
+            'regex' => 'O :attribute deve estar no formato HH:mm', // Feedback para horarioInicio e horarioFim.
+            'before_or_equal' => 'O :attribute deve ser anterior ou igual a :other', // Feedback para horarioInicio.
+            'after_or_equal' => 'O :attribute deve ser posterior ou igual a :other', // Feedback para horarioFim.
+            'min' => 'O :attribute deve ter pelo menos :min caracteres',
+        ];
+    }
+
     public function fair()
     {
         return $this->belongsTo(Fair::class);
