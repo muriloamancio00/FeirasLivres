38a9fdae34deffa89c6ac916e1b2a646ca779fb5
murilo <61051754+muriloamancio00@users.noreply.github.com>
2023-10-03 00:51:22 -0300

    Estrutura Fair.vue

--- resources/js/components/FairComponent.vue
+++ resources/js/components/FairComponent.vue
@@ -2,22 +2,21 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <!-- Inicio do card de Busca -->
+                <!-- Inicio Busca -->
                 <card-component titulo="Busca de Feiras">
                     <template v-slot:conteudo>
                         <div class="form-row">
-                            <div class="col mb-3">
+                            <div class="col">
                                 <encapsular-component
                                     titulo="ID"
                                     id="InputId"
                                     id-help="idHelp"
-                                    texto-ajuda="Opcional. Informe o ID da Feira">
+                                    texto-ajuda="Opcional. Informe o ID do Feira">
                                     <input type="number" class="form-control" id="inputId" aria-describedby="idHelp"
-                                           placeholder="ID da feira">
+                                           placeholder="ID do Feira" v-model="busca.id">
                                 </encapsular-component>
                             </div>
-
-                            <div class="col mb-3">
+                            <div class="col">
                                 <encapsular-component
                                     titulo="Nome da Feira"
                                     id="InputNome"
@@ -25,54 +24,87 @@
                                     texto-ajuda="Opcional. Informe o Nome da Feira">
 
                                     <input type="text" class="form-control" id="inputNome" aria-describedby="NomeHelp"
-                                           placeholder="Nome da feira">
-
+                                           placeholder="Nome da Feira" v-model="busca.nome">
                                 </encapsular-component>
                             </div>
                         </div>
                     </template>
                     <template v-slot:rodape>
-                        <button type="button" class="btn btn-primary btn-sm float-right " data-toggle="modal" data-target="#modalFeira">Adicionar</button></template>
+                        <button type="button" class="btn btn-primary btn-sm float-right" @click="pesquisar()">Pesquisar</button></template>
                 </card-component>
-                <!-- Fim do card de Busca -->
+                <!-- Fim Busca -->
 
-                <!-- Inicio do card de Listagem de Feiras -->
-                <card-component titulo="Todas as Feiras">
-                    <template v-slot:conteudo>
-                        <table-component></table-component>
-                    </template>
+                <!-- Inicio Lista -->
 
-                    <template v-slot:rodape>
-                        <button type="button" class="btn btn-primary btn-sm float-right">Adicionar</button>
-                    </template>
-                </card-component>
-                <!-- Fim da Listagem de Feiras -->
+                <!-- Fim Lista -->
+
             </div>
         </div>
-        <!-- Button trigger modal -->
-        <modal-component id="modalFeira" titulo="Adicionar Feira">
-            <template v-slot:conteudo>
-                <div class="form-group">
-                    <encapsular-component titulo="Nome da Feira" id="novoNome" id-help="novoNomeHelp" texto-ajuda="Informe o nome da feira">
-                        <input type="text" class="form-control" id="novoNome" aria-describedby="novoNomeHelp"
-                               placeholder="Nome da feira">
-                    </encapsular-component>
-                </div>
-            </template>
-            <template v-slot:rodape>
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
-                <button type="button" class="btn btn-primary">Salvar</button>
-            </template>
-        </modal-component>
-    </div>
 
+        <!-- Inicio Modal Adiciona -->
+
+        <!-- Fim Modal Adiciona -->
+
+
+        <!-- Inicio Modal Visualizar -->
+
+        <!-- Fim Modal Visualizar -->
+
+
+        <!-- Inicio modal Remove -->
+
+        <!-- Fim modal Remove -->
+
+
+        <!-- Inicio Modal Alterar -->
+
+        <!-- Fim Modal Alterar -->
+
+    </div>
 </template>
 
 <script>
-    import {defineComponent} from "vue";
-    import Card from "./CardComponent.vue";
+export default {
+    data() {
+        return {
+            urlBase: 'http://127.0.0.1:8000/api/v1/fair/',
+            urlPaginacao: '',
+            urlFiltro: '',
+            //atributos
+            nomeFeira: '',
+            enderecoFeira: '',
+            horarioInicioFeira: '',
+            horarioFimFeira: '',
+            longitudeFeira: '',
+            latitudeFeira: '',
+            //variavel para salvar o estado da instancia do vue
+            transacaoStatus: '',
+            transacaoDetalhes: {},
+            //definindo para vazio, ao invez de indefinido antes do meto assincrono for realizado
+            feiras: {data: []},
+            busca: {id: '', nome: ''},
+        }
+    },
+    methods: {
+        carregarLista() {
+            let url = this.urlBase +'?' + this.urlPaginacao + this.urlFiltro
 
-    export default defineComponent({
-        components: {Card}
-    })
+            axios.get(url)
+                .then(response => {
+                    this.feiras = response.data
+                })
+                .catch(errors => {
+                    console.log(errors)
+                })
+        },
+        paginacao(l){},
+        salvar() {},
+        atualizar(){},
+        remover() {},
+        pesquisar(){},
+    },
+    mounted(){
+        this.carregarLista()
+    }
+}
 </script>
