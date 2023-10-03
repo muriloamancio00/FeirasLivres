6472eeabb5fa8dd0326bb369b1a6e1074e84ea92
murilo <61051754+muriloamancio00@users.noreply.github.com>
2023-10-02 23:13:43 -0300

    Creat Event

--- resources/js/components/EventComponent.vue
+++ resources/js/components/EventComponent.vue
@@ -3,18 +3,103 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <!-- Inicio Busca -->
+                <card-component titulo="Busca de Eventos">
+                    <template v-slot:conteudo>
+                        <div class="form-row">
+                            <div class="col mb-3">
+                                <encapsular-component
+                                    titulo="ID"
+                                    id="InputId"
+                                    id-help="idHelp"
+                                    texto-ajuda="Opcional. Informe o ID do Evento">
+                                    <input type="number" class="form-control" id="inputId" aria-describedby="idHelp"
+                                           placeholder="ID do Evento" v-model="busca.id">
+                                </encapsular-component>
+                            </div>
+                        </div>
+                        <encapsular-component
+                            titulo="Nome do Evento"
+                            id="InputNome"
+                            id-help="NomeHelp"
+                            texto-ajuda="Opcional. Informe o Nome do Evento">
 
+                            <input type="text" class="form-control" id="inputNome" aria-describedby="NomeHelp"
+                                   placeholder="Nome do Evento" v-model="busca.nome">
+                        </encapsular-component>
+                    </template>
+                    <template v-slot:rodape>
+                        <button type="button" class="btn btn-primary btn-sm float-right" @click="pesquisar()">Pesquisar</button></template>
+                </card-component>
                 <!-- Fim Busca -->
 
                 <!-- Inicio Lista -->
+                <card-component titulo="Todos os Evento">
+                    <template v-slot:conteudo>
+                        <!--os dados precisma ser um array de obj, e os titulos coincidir com os atributos do obj-->
+                        <table-component
+                            :dados="eventos.data"
+                            :visualizar="{ visivel: true, dataToggle: 'modal', dataTarget: '#modalEventoVizualizar'}"
+                            :atualizar="{ visivel: true, dataToggle: 'modal', dataTarget: '#modalEventoAtualizar'}"
+                            :remover="{ visivel: true, dataToggle: 'modal', dataTarget: '#modalEventoRemover'}"
+                            :titulos="{
+                                id: {titulo: 'ID', tipo:'text'},
+                                nome: {titulo: 'Nome', tipo:'text'},
+                                descricao: {},
+                                created_at: {},
+                            }"
+                        ></table-component>
+                    </template>
 
+                    <template v-slot:rodape>
+                        <div class="row">
+                            <div class="col-10">
+                                <paginate-component>
+                                    <!-- percorrendo todos os links dentro-->
+                                    <li v-for="l, key in eventos.links" :key="key"
+                                        :class="l.active ? 'page-item active' : 'page-item'"
+                                        @click="paginacao(l)"
+                                    >
+                                        <a class="page-link" v-html="l.label">
+                                        </a>
+                                    </li>
+                                </paginate-component>
+                            </div>
+                            <div class="col">
+                                <button type="button" class="btn btn-primary btn-sm float-right" data-toggle="modal" data-target="#modalEvento">Adicionar</button>
+                            </div>
+                        </div>
+                    </template>
+                </card-component>
                 <!-- Fim Lista -->
 
             </div>
         </div>
 
         <!-- Inicio Modal Adiciona -->
+        <modal-component id="modalEvento" titulo="Adicionar Evento">
 
+            <template v-slot:alerta>
+                <alert-component tipo="success" :detalhes="transacaoDetalhes" titulo="Cadastro realizado com sucesso!" v-if="transacaoStatus == 'adicionado'"></alert-component>
+                <alert-component tipo="danger" :detalhes="transacaoDetalhes" titulo="Erro, Evento não cadastrado!" v-if="transacaoStatus == 'erro'"></alert-component>
+            </template>
+            <template v-slot:conteudo>
+                <div class="form-group">
+                    <encapsular-component titulo="Nome do Evento" id="novoNome" id-help="novoNomeHelp" texto-ajuda="Informe o Nome do Evento">
+                        <input type="text" class="form-control" id="novoNome" aria-describedby="novoNomeHelp"
+                               placeholder="Nome do Evento" v-model:id="nomeEvento">
+                    </encapsular-component>
+                    <hr><p>Opcional</p>
+                    <encapsular-component titulo="Descrição do Evento" id="novoDescricao" id-help="novoDescricaoHelp" texto-ajuda="Informe a Descricao do Evento">
+                        <input type="text" class="form-control" id="novoDescricao" aria-describedby="novoDescricaoHelp"
+                               placeholder="Opcional. Descricao do Evento" v-model:id="descricaoEvento">
+                    </encapsular-component>
+                </div>
+            </template>
+            <template v-slot:rodape>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+                <button type="button" class="btn btn-primary" @click="salvar()">Salvar</button>
+            </template>
+        </modal-component>
         <!-- Fim Modal Adiciona -->
 
 
@@ -38,12 +123,50 @@
 <script>
 export default {
     data() {
-        return{}
+        return {
+            urlBase: 'http://127.0.0.1:8000/api/v1/event/',
+            urlPaginacao: '',
+            urlFiltro: '',
+            nomeEvento: '',
+            descricaoEvento: '',
+            //variavel para salvar o estado da instancia do vue
+            transacaoStatus: '',
+            transacaoDetalhes: {},
+            //definindo para vazio, ao invez de indefinido antes do meto assincrono for realizado
+            eventos: {data: []},
+            busca: {id: '', nome: ''},
+        }
     },
     methods: {
         carregarLista() {},
         paginacao(l){},
-        salvar() {},
+        salvar() {
+            let config = {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                }
+            }
+            let formData = new FormData();
+            formData.append('nome', this.nomeEvento)
+            formData.append('descricao', this.descricaoEvento)
+
+            axios.post(this.urlBase, formData, config)
+                .then(response => {
+                    //fluxo cadastro correto
+                    this.transacaoStatus = 'adicionado'
+                    this.transacaoDetalhes = {
+                        mensagem: 'ID do registro: ' + response.data.id
+                    }
+                    this.carregarLista()
+                })
+                .catch(errors => {
+                    this.transacaoStatus = 'erro'
+                    this.transacaoDetalhes = {
+                        mensagem: errors.response.data.message,
+                        dados: errors.response.data.errors
+                    }
+                })
+        },
         atualizar(){},
         remover() {},
         pesquisar(){},
