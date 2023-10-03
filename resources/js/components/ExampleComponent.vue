e09dd5707b5ab7d390c25fc8c123a4add77c7eab
murilo <61051754+muriloamancio00@users.noreply.github.com>
2023-10-02 21:22:51 -0300

    Update ExampleComponent.vue

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -2,22 +2,54 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Example Component</div>
+                <!-- Inicio Busca -->
 
-                    <div class="card-body">
-                        I'm an example component.
-                    </div>
-                </div>
+                <!-- Fim Busca -->
+
+                <!-- Inicio Lista -->
+
+                <!-- Fim Lista -->
+
             </div>
         </div>
+
+        <!-- Inicio Modal Adiciona -->
+
+        <!-- Fim Modal Adiciona -->
+
+
+        <!-- Inicio Modal Visualizar -->
+
+        <!-- Fim Modal Visualizar -->
+
+
+        <!-- Inicio modal Remove -->
+
+        <!-- Fim modal Remove -->
+
+
+        <!-- Inicio Modal Alterar -->
+
+        <!-- Fim Modal Alterar -->
+
     </div>
 </template>
 
 <script>
-    export default {
-        mounted() {
-            console.log('Component mounted.')
-        }
+export default {
+    data() {
+        return{}
+    },
+    methods: {
+        carregarLista() {},
+        paginacao(l){},
+        salvar() {},
+        atualizar(){},
+        remover() {},
+        pesquisar(){},
+    },
+    mounted(){
+        this.carregarLista()
     }
+}
 </script>
